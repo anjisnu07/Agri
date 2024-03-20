b399af598e6e0dea2ef6a878d56bdd436df787cf
Anjisnu Roy <royanjisnu78@gmail.com>
2024-03-21 01:49:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import os
 from utils.fertilizer import fertilizer_dic
 
-crop_recommendation_model_path = '../models/RandomForest.pkl'
+crop_recommendation_model_path = 'RandomForest.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
